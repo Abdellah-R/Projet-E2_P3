43ab796d1e148fe8b104e4ab7c76390f3f577e1f
Abdellah <abdodo59@gmail.com>
2023-08-15 14:45:39 +0200

    feat(views): :sparkles: Création des vues index et predict

--- real_estate_app/main_app/views.py
+++ real_estate_app/main_app/views.py
@@ -1,28 +1,48 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .utils import make_prediction
+from .models import PredictionForm
 
+fields_group1 = ['Year_Built', 'Total_Bsmt_SF', 'First_Flr_SF', 'Gr_Liv_Area', 'Garage_Area']
+fields_group2 = ['Overall_Qual', 'Full_Bath', 'Exter_Qual', 'Kitchen_Qual', 'Neighborhood']
 
 def index(request):
-    return render(request, 'index.html')
+
+    form = PredictionForm()
+    
+
+    context = {'form': form,
+               'fields_group1': [form[field] for field in fields_group1],
+               'fields_group2': [form[field] for field in fields_group2]
+            }
+    
+    return render(request, 'index.html', context)
 
 
 def predict(request):
-    if request.method == 'POST':
-        X_predict = {}
-        for var in [
-                'Year_Built', 'Total_Bsmt_SF', '1st_Flr_SF', 'Gr_Liv_Area',
-                'Garage_Area', 'Overall_Qual', 'Full_Bath', 'Exter_Qual',
-                'Kitchen_Qual', 'Neighborhood']:
-            if var in ['Exter_Qual', 'Kitchen_Qual', 'Neighborhood']:
-                X_predict[var] = request.POST.get(var)
-            else:
-                X_predict[var] = int(request.POST.get(var))
-        pred = make_prediction(X_predict)
 
-        if pred != 0:
-            return render(request, 'index.html', {'data': int(pred)})
+    if request.method == 'POST':
+        form = PredictionForm(request.POST)  # Utilisez votre modèle de formulaire
+        
+        if form.is_valid():
+            X_predict = form.cleaned_data  # Utilisez les données propres et validées du formulaire
+        
+            pred = make_prediction(X_predict)
+
+            if pred != 0:
+                context = {'form': form,
+                           'fields_group1': [form[field] for field in fields_group1],
+                           'fields_group2': [form[field] for field in fields_group2],
+                           'data': int(pred)
+                        }
+                
+                return render(request, 'index.html', context)
+            else:
+                return HttpResponse("Invalid input", status=400)
+        
         else:
-            return HttpResponse("The Input is not Correct")
+            return HttpResponse("Invalid input", status=400)
+        
     else:
         return HttpResponse("Method Not Allowed")
+
